c4fd04083927a08b68091bd5e57abc28f579d80d
AbdElRahman Nabil <abdelrahmann89@gmail.com>
2021-03-02 16:05:34 +0200

    AN: added getLocation to the home fragment

--- app/src/main/java/com/example/weatherforecast/model/Weather.kt
+++ app/src/main/java/com/example/weatherforecast/model/Weather.kt
@@ -1,5 +1,7 @@
 package com.example.weatherforecast.model
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 
 
@@ -28,4 +30,37 @@ data class Weather(
 
 	@field:SerializedName("lat")
 	val lat: Double? = null
-)
+) : Parcelable {
+	constructor(parcel: Parcel) : this(
+		TODO("alerts"),
+		TODO("current"),
+		parcel.readString(),
+		parcel.readValue(Int::class.java.classLoader) as? Int,
+		TODO("daily"),
+		parcel.readValue(Double::class.java.classLoader) as? Double,
+		TODO("hourly"),
+		parcel.readValue(Double::class.java.classLoader) as? Double
+	) {
+	}
+
+	override fun writeToParcel(parcel: Parcel, flags: Int) {
+		parcel.writeString(timezone)
+		parcel.writeValue(timezoneOffset)
+		parcel.writeValue(lon)
+		parcel.writeValue(lat)
+	}
+
+	override fun describeContents(): Int {
+		return 0
+	}
+
+	companion object CREATOR : Parcelable.Creator<Weather> {
+		override fun createFromParcel(parcel: Parcel): Weather {
+			return Weather(parcel)
+		}
+
+		override fun newArray(size: Int): Array<Weather?> {
+			return arrayOfNulls(size)
+		}
+	}
+}
--- app/src/main/java/com/example/weatherforecast/view/HomeFragment.kt
+++ app/src/main/java/com/example/weatherforecast/view/HomeFragment.kt
@@ -1,30 +1,58 @@
 package com.example.weatherforecast.view
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
+import android.provider.Settings
 import android.text.Layout
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.weatherforecast.R
 import com.example.weatherforecast.databinding.FragmentHomeBinding
 import com.example.weatherforecast.model.Constants
+import com.example.weatherforecast.model.Weather
 import com.example.weatherforecast.utilities.Utility
 import com.example.weatherforecast.viewmodel.HomeViewModel
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.model.LatLng
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import java.text.DateFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    lateinit var fusedLocationProviderClient : FusedLocationProviderClient
+    val permissionID=7
+    lateinit var latLng: LatLng
+    lateinit var done:MutableLiveData<Boolean>
     private var _binding: FragmentHomeBinding? = null
     // This property is only valid between onCreateView and
 // onDestroyView.
     private val binding get() = _binding!!
-    lateinit var calendar: Calendar
+    var weather: Weather?=null
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -39,8 +67,17 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         super.onViewCreated(view, savedInstanceState)
         val viewModel=ViewModelProvider(this).get(HomeViewModel::class.java)
         val simpleDateFormat = SimpleDateFormat("hh:mm");
-        viewModel.fetchData(40.712776,-74.005974)
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireActivity())
+        done=MutableLiveData()
+        done.postValue(false)
+        getLocation()
+        done.observe(viewLifecycleOwner,{
+            if(it) {
+                viewModel.fetchData(latLng.latitude, latLng.longitude)
+            }
+        })
         viewModel.weatherLiveDate.observe(viewLifecycleOwner,{
+            weather=it
             binding.tempTV.text=it.current?.temp.toString()+"F"
             binding.dateTV.text=DateFormat.getDateInstance().format(Utility.getCurrentDateAndTime().time)
             binding.timeTV.text=simpleDateFormat.format(Utility.getCurrentDateAndTime().time)
@@ -55,13 +92,116 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
         })
         binding.weekForecast.setOnClickListener {
-        val action=HomeFragmentDirections.actionHomeFragmentToDaysFragment()
-            findNavController().navigate(action)
+            if(weather!=null){
+        val action=HomeFragmentDirections.actionHomeFragmentToDaysFragment(weather!!)
+                findNavController().navigate(action)
+            }
         }
 
     }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getLocation(){
+        if(isPermissionsGranted()){
+            if (isLocationEnabled()){
+                fusedLocationProviderClient.lastLocation.addOnCompleteListener {
+                    requestLocation()
+                }
+            } else{
+                enableLocation()
+            }
+        }else{
+            requestPermission()
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun requestLocation(){
+        val locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 100
+        //locationRequest.numUpdates=1
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private var locationCallback=object: LocationCallback(){
+        override fun onLocationResult(p0: LocationResult?) {
+            super.onLocationResult(p0)
+            var locationRequest=p0?.lastLocation
+            latLng= LatLng(locationRequest!!.latitude, locationRequest!!.longitude)
+            done.postValue(true)
+
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager = activity?.getSystemService(AppCompatActivity.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun enableLocation(){
+        Toast.makeText(activity, "could you enable your location?", Toast.LENGTH_SHORT).show()
+        val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+        startActivity(intent)
+    }
+
+    private fun isPermissionsGranted(): Boolean {
+        return if (ActivityCompat.checkSelfPermission(
+                requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            true
+        } else {
+            false
+        }
+    }
+
+    //request permission for accessing location
+    private fun requestPermission(){
+        var permissions= arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+        ActivityCompat.requestPermissions(requireActivity(), permissions as Array<String>, permissionID)
+
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<out String>, grantResults: IntArray
+    ) {
+        if (requestCode==permissionID){
+            if (grantResults[0]== PackageManager.PERMISSION_GRANTED){
+                getLocation()
+            }
+            else{
+                warningDialog()
+            }
+        }
+    }
+
+    private fun warningDialog(){
+        val builder = AlertDialog.Builder(requireActivity())
+        builder.setTitle("Warning")
+        builder.setMessage(
+            "without this permission the app will not be able to work properly!\n " +
+                    "Would you like to give us the permission?"
+        )
+
+        builder.setPositiveButton("Yes") { dialog, which ->
+            requestPermission()
+        }
+        builder.setNegativeButton("No") { dialog, which ->
+        }
+        builder.show()
     }
 }
